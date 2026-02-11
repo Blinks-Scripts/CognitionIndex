33e8e2947f313e5e4baa7bf03b3a8429d9045f90
Jeffrey Blinks <jeffblinks@gmail.com>
2026-02-10 19:05:33 -0600

    Moved it all - again?

--- src/components/TextTranscription.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const TextTranscription: React.FC<{ text: string }> = ({ text }) => {
-  return (
-    <div>
-      <p>{text}</p>
-    </div>
-  );
-};
--- src/views/Settings.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Settings: React.FC<{ id: string; label: string }> = ({ id, label }) => {
-  return (
-    <div>
-      <h1>{label}</h1>
-    </div>
-  );
-};
